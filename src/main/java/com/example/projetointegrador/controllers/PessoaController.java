8f558556984b1c0a5b0f8081f31e3cb47bddd3bd
IVAN1401 <ivan.0234@aluno.pr.senac.br>
2022-11-01 22:21:17 -0300

    buscarpessoa

---
+++ src/main/java/com/example/projetointegrador/controllers/PessoaController.java
@@ -0,0 +1,37 @@
+package com.example.projetointegrador.controllers;
+
+
+import com.example.projetointegrador.models.Pessoa;
+import com.example.projetointegrador.services.PessoaServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin
+public class PessoaController {
+
+    final PessoaServiceImpl pessoaServiceImp;
+
+    public PessoaController(PessoaServiceImpl pessoaService) {
+        this.pessoaServiceImp = pessoaService;
+    }
+    @PostMapping(value= "/salvarPessoa")
+    public ResponseEntity<Object> salvarPessoa(@RequestBody Pessoa pessoa) {
+       Pessoa response = pessoaServiceImp.salvar(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping(value = "/buscarPessoa")
+    public ResponseEntity<Object> buscarPessoa(@RequestBody Pessoa pessoa) {
+       List<Pessoa>  response = pessoaServiceImp.listar();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+        @GetMapping(value = "/alterarPessoa")
+        public ResponseEntity<Object> alterarPessoa (@RequestBody Pessoa pessoa){
+            Pessoa response = pessoaServiceImp.editar(pessoa);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
+    }
